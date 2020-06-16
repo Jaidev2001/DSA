d87e582e7c494116f35a4ff3dc6905f9ca448cee
Jaidev2001 <jaidevbehera2001@gmail.com>
2020-06-16 16:22:48 +0530

    *_*

---
+++ .vscode/binarysearch2.cpp
@@ -0,0 +1,40 @@
+//BINARY SEARCH FOR First left element SORTED ARRAY*********
+#include<iostream>
+ using namespace std;
+int search(int arr[],int l,int h,int x)
+{
+    int mid=l+(h-l)/2;
+    if(l>h)
+        return 0;
+    if(arr[mid]==x&&arr[mid-1]!=x)
+    {
+        cout<<"index ["<<mid<<"] :";
+        return 1;
+    }
+    else if(x<=arr[mid])
+        return search(arr,l,mid-1,x);
+    else
+    {
+        return search(arr,mid+1,h,x);
+    }
+    
+    
+}
+ int main()
+ {
+    int arr[]={1, 3, 3, 3, 6};
+    int x;
+    
+    cin>>x;
+    if(search(arr,0,4,x))
+    {
+        cout<<"Found the element "<<x;
+    }
+    else
+    {
+        cout<<"NOt Found the element "<<x;
+
+    }
+    
+    return 0;
+ }
